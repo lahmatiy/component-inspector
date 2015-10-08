747ece812dd9c3e2d7ff64174d61cfab5ca84ef1
Sergey Lapin <lapanoid@gmail.com>
2015-10-08 22:40:28 +0300

    add react todomvc example

---
+++ examples/react-todomvc/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import TodoApp from './TodoApp';
+import { createStore, combineReducers, compose } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from '../reducers';
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
+export default class App extends Component {
+  render() {
+    return (
+        <Provider store={store}>
+          {() => <TodoApp /> }
+        </Provider>
+    );
+  }
+}
+
+
---
+++ examples/react-todomvc/webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  babel: {
+    plugins: [
+      // in case you are using React, this plugin should be applied
+      // before babel-plugin-source-wrapper
+      // otherwise component names will not to be shown propertly
+      require('babel-plugin-react-display-name'),
+      require('babel-plugin-source-wrapper').configure({
+        // webpack sends absolute paths to plugins
+        // but we need paths relative to project root
+        basePath: process.cwd(),
+
+        // inject runtime in instrumented sources
+        runtime: true
+      })
+    ]
+  },
+  plugins: [
+
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
+  resolve: {
+    alias: {
+      'react': path.join(__dirname, 'node_modules', 'react')
+    },
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    }, {
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, '..', '..', 'src')
+    }, {
+      test: /\.css?$/,
+      loaders: ['style', 'raw'],
+      include: __dirname
+    }]
+  }
+};
