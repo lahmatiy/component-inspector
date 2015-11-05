2af9b19a046caa2f4241e22b60abd72467c11c6b
Roman Dvornov <rdvornov@gmail.com>
2015-11-05 23:27:56 +0300

    use react 0.14.2 for todomvc example

--- examples/react-todomvc/containers/App.js
+++ examples/react-todomvc/containers/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
   render() {
     return (
         <Provider store={store}>
-          {() => <TodoApp /> }
+          <TodoApp />
         </Provider>
     );
   }
