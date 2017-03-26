48af4fe3a4768448f5e379bb666f1898f87ea156
Roman Dvornov <rdvornov@gmail.com>
2017-03-26 04:02:36 +0300

    fix building and rework subscriber distribution

--- src/inspector-rempl/publisher/remote.js
+++ src/inspector-rempl/publisher/remote.js
@@ -1,19 +1,10 @@
 var rempl = require('rempl');
-var INSPECTOR_URL = asset('/src/inspector-rempl/subscriber/index.html');
-var INSPECTOR_BUILD_URL = asset('./unknown');
 
 module.exports = rempl.createPublisher('Component Inspector', function(settings, callback) {
   // jscs:disable
-  /** @cut */ if (settings.dev) {
-  /** @cut */   return callback(null, 'url', 'http://localhost:8000' + INSPECTOR_URL);
-  /** @cut */ }
+  /** @cut */ return callback(null, 'url', 'http://localhost:8000/src/inspector-rempl/subscriber/index.html');
   // jscs:enable
 
-  global.basisjsToolsFileSync.getBundle(settings.dev ? INSPECTOR_URL : {
-    build: INSPECTOR_BUILD_URL,
-    filename: INSPECTOR_URL
-  }, function(err, script) {
-    callback(err, 'script', script);
-  });
+  callback(null, 'script', asset('/dist/subscriber.js', true));
 });
 
--- src/react.js
+++ src/react.js
@@ -1,6 +1,6 @@
-var initInspector = require('./inspector/index.js');
+// var initInspector = require('./inspector/index.js');
 
 require('./api-adapter/react.js').ready(function(api) {
-  initInspector(api);
+  // initInspector(api);
   return require('./inspector-rempl/publisher/index.js')(api);
 });
