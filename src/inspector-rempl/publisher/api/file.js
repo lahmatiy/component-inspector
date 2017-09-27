a9ff0b37599c77f93405d2c5e12d8c8966679d53
Roman Dvornov <rdvornov@gmail.com>
2017-09-27 19:07:32 +0300

    refactor open file handling

--- src/inspector-rempl/publisher/api/file.js
+++ src/inspector-rempl/publisher/api/file.js
@@ -1,43 +1,36 @@
-var openFileUrl;
+/* global OPEN_FILE_URL, basisjsToolsFileSync */
+
 var openFile;
 
 function isOpenFileSupported() {
-  if (openFile) {
+  if (typeof openFile === 'function') {
     return true;
   }
 
-  if (typeof OPEN_FILE_URL === 'string') {
-    // eslint-disable-next-line no-undef
-    openFileUrl = OPEN_FILE_URL;
-    openFile = openFileByUrl;
-    return true;
-  }
-
-  // eslint-disable-next-line no-undef
-  var basisjsTools = typeof basisjsToolsFileSync !== 'undefined' ? basisjsToolsFileSync : basis.devtools;
-  if (basisjsTools && typeof basisjsTools.openFile == 'function') {
+  var openFileUrl = typeof OPEN_FILE_URL === 'string' ? OPEN_FILE_URL : false;
+  if (openFileUrl) {
     openFile = function(filename) {
-      openFileByBasisjsTools(basisjsTools, filename);
+      var xhr = new XMLHttpRequest();
+      var url = OPEN_FILE_URL +
+                (/\?/.test(OPEN_FILE_URL) ? '&' : '?') +
+                'file=' + escape(filename);
+
+      // async GET
+      xhr.open('GET', url, true);
+      // set if-modified-since header since begining prevents cache using
+      xhr.setRequestHeader('If-Modified-Since', new Date(0).toGMTString());
+      xhr.send('');
     };
     return true;
   }
-}
 
-function openFileByBasisjsTools(host, filename) {
-  host.openFile(basis.path.resolve(filename.replace(/(:\d+:\d+):\d+:\d+$/, '$1')));
-}
-
-function openFileByUrl(filename) {
-  var xhr = new XMLHttpRequest();
-  var url = openFileUrl +
-            (/\?/.test(openFileUrl) ? '&' : '?') +
-            'file=' + escape(filename);
-
-  // async GET
-  xhr.open('GET', url, true);
-  // set if-modified-since header since begining prevents cache using
-  xhr.setRequestHeader('If-Modified-Since', new Date(0).toGMTString());
-  xhr.send('');
+  var basisjsTools = typeof basisjsToolsFileSync !== 'undefined' ? basisjsToolsFileSync : basis.devtools;
+  if (basisjsTools && typeof basisjsTools.openFile == 'function') {
+    openFile = function(filename) {
+      basisjsTools.openFile(basis.path.resolve(filename.replace(/(:\d+:\d+):\d+:\d+$/, '$1')));
+    };
+    return true;
+  }
 }
 
 module.exports = {
