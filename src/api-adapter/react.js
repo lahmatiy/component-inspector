5d834077ab4ed3d83dcc3fb78f3f79e5d134b6a4
Roman Dvornov <rdvornov@gmail.com>
2017-11-21 01:47:55 +0300

    rework react API entry point to not rely on basis.js

--- src/api-adapter/react.js
+++ src/api-adapter/react.js
@@ -1,27 +1,39 @@
 var createApi = require('./react/create-api.js');
-var api = new basis.Token();
+var subsciptions = [];
+var reactApis = [];
 
-function adoptAPI(reactApi) {
-  // export API
-  api.set(createApi(reactApi));
-}
 // attach to React devtools
 var oldHook = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
 if (oldHook) {
   var oldInject = oldHook.inject;
   oldHook.inject = function(api) {
-    adoptAPI(api);
+    notify(api);
     oldInject.apply(this, arguments);
   };
 } else {
   window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {
-    inject: adoptAPI
+    inject: notify
   };
+}
+
+// notify subscribers
+function notify(api) {
+  api = createApi(api);
+  reactApis.push(api);
+  subsciptions.forEach(function(fn) {
+    fn(api);
+  });
 }
 
 // provide ready function
 module.exports = {
   ready: function(fn) {
-    api.attach(fn);
+    subsciptions.push(fn);
+    reactApis.forEach(function(api) {
+      // use setTimeout for consistent behaviour
+      setTimeout(function() {
+        fn(api);
+      }, 0);
+    });
   }
 };
